208e09aa5648bfe5bf832c2641d57ac8f5446e48
rngrhn4114 <leesy020411@sookmyung.ac.kr>
2025-01-26 01:53:05 +0900

    [chore]: 코드 정리

--- app/components/common/toast.tsx
+++ app/components/common/toast.tsx
@@ -1,86 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 import Image from 'next/image.js';
-import burnt1 from '../../../public/burnt1.png';
+import { useState, useEffect } from 'react';
 
-interface ToastProps {
-  index: number;
-  title: string;
-  content: string;
-}
+import Toast1 from '../../assets/toasts/toast1.png';
+import Toast2 from '../../assets/toasts/toast2.png';
+import Toast3 from '../../assets/toasts/toast3.png';
+import Toast4 from '../../assets/toasts/toast4.png';
+import Toast5 from '../../assets/toasts/toast5.png';
 
-const Toast: React.FC<ToastProps> = ({ index, title, content }) => {
-  return (
-    <ImageWrapper key={index}>
-      <Image
-        src={burnt1}
-        alt="burnt"
-        layout="fixed"
-        width={145}
-        height={145}
-        objectFit="cover"
-      />
-      <TextOverlay>
-        <ContentText>{content}</ContentText>
-      </TextOverlay>
-      <TitleText>{title}</TitleText>
-    </ImageWrapper>
-  );
+const Toasts = styled(Image)`
+  display: block;
+  cursor: pointer; /* 클릭 가능하게 설정 */
+`;
+
+const toastImages = [Toast1, Toast2, Toast3, Toast4, Toast5];
+
+const Toast: React.FC = () => {
+  const [toastNumber, setToastNumber] = useState<string>(toastImages[0].src);
+
+  useEffect(() => {
+    // 랜덤으로 이미지 선택
+    const randomToast = toastImages[Math.floor(Math.random() * toastImages.length)];
+    setToastNumber(randomToast.src);
+  }, []); // 컴포넌트가 처음 렌더링될 때 실행
+
+  return <Toasts src={toastNumber} alt="RandomToast" width={296} height={320} />;
 };
 
 export default Toast;
-
-const ImageWrapper = styled.div`
-  position: relative;
-  height: auto;
-`;
-
-const TextOverlay = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 60%;
-  width: 70%; /* 고정된 너비 */
-  overflow-y: hidden;
-  text-align: left;
-  table-layout: fixed;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: white; /* 텍스트 색상 */
-  margin: 20px;
-`;
-
-const ContentText = styled.div`
-  width: 104px;
-  height: 120px;
-
-  font-family: 'SUIT';
-  font-style: normal;
-  font-weight: 600;
-  font-size: 11px;
-  line-height: 12px;
-  /* or 109% */
-
-  /* mocha category */
-  color: #501500;
-
-  mix-blend-mode: plus-darker;
-  /* burnt_text */
-
-  /* Inside auto layout */
-  flex: none;
-  order: 0;
-  align-self: stretch;
-  flex-grow: 1;
-`;
-
-const TitleText = styled.div`
-  font-family: 'SUIT';
-  font-style: normal;
-  font-weight: 800;
-  font-size: 16px;
-  /* or 125% */
-  text-align: center;
-  color: black;
-`;
