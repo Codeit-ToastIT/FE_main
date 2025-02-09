81069435d6ee57526a10372915e422b294b03048
rngrhn4114 <leesy020411@sookmyung.ac.kr>
2025-02-09 18:32:26 +0900

    [chore]: 백엔드 배포 링크 수정

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://52.79.251.171:8000';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://api.toast-it.site';
